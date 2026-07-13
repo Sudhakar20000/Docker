d7a6c27cc106c8d26922168f36cb65cdd94b06eb
Sudhakar20000 <mandahemasudhakar@gmail.com>
2026-07-13 12:41:39 +0530

    docker-compose

--- docker_infra/main.tf
+++ docker_infra/main.tf
@@ -16,6 +16,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.web_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_out" {
   security_group_id = aws_security_group.web_sg.id
   cidr_ipv4         = "0.0.0.0/0"
